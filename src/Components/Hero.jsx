a709645ab2a9f7bb2661e803e962edfdadc62965
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-30 03:05:26 +0600

    `Update import statements in Hero and Navbar components`

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "./Modal";
-import image from "/Hero/hero.jpg";
+import image from "/Hero/hero1.jpg";
 
 const Hero = () => {
   const navigate = useNavigate();
